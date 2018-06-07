115399dc4fd38ce2e9467a76fc356084d7997f8c
zouqf <zouqf@317hu.com>
2018-06-07 11:06:55 +0800

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by MacBookPro on 2018/6/7.
+//  Copyright © 2018年 317hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
